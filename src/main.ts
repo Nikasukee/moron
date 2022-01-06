c3d5d99e3c96b571fbb624db8de84f485a48761e
LeXonJe <30697846+LeXonJe@users.noreply.github.com>
2022-01-06 17:03:03 +0100

    Added Discord, Dotenv and basic bot logic

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,13 @@
-export default function sayHello(name: string) {
-  return `Hello, ${name}`;
-}
+import { config as env } from "dotenv";
+import { Client, Intents } from "discord.js";
+
+// Load environment variables to get Discord Token
+env();
+
+console.log("Bot is starting...");
+
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS],
+});
+
+client.login(process.env.DISCORD_TOKEN);
